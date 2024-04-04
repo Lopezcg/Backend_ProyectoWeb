fd3971c1d7a2546b5865db44242f032a7221dd1a
joseManuel1234 <josemrodriguez@javeriana.edu.co>
2024-04-03 23:00:54 -0500

    nose

--- src/test/java/com/backend/backend_web/CalificacionServiceTest.java
+++ src/test/java/com/backend/backend_web/CalificacionServiceTest.java
@@ -1,24 +1,38 @@
-package com.backend.backend_web.service;
+package com.backend.backend_web;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-import org.modelmapper.ModelMapper;
-import java.util.Optional;
-import com.backend.backend_web.repository.CalificacionRepository;
-import com.backend.backend_web.entity.Calificacion;
-import com.backend.backend_web.dto.CalificacionDTO;
-import com.backend.backend_web.service.CalificacionService;
+
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
-public class CalificacionServiceTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
 
+import com.backend.backend_web.dto.CalificacionDTO;
+import com.backend.backend_web.entity.Calificacion;
+import com.backend.backend_web.repository.CalificacionRepository;
+import com.backend.backend_web.service.CalificacionService;
+
+@ExtendWith(MockitoExtension.class)
+class CalificacionServiceTest {
+
+    @Mock
     private CalificacionRepository repositoryMock;
+    
+    @Mock
     private ModelMapper modelMapperMock;
+    
+    @InjectMocks
     private CalificacionService service;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         repositoryMock = mock(CalificacionRepository.class);
         modelMapperMock = mock(ModelMapper.class);
@@ -29,10 +43,9 @@ public class CalificacionServiceTest {
     public void testGetCalificacionExists() {
         Long id = 1L;
         Calificacion calificacion = new Calificacion();
-        Optional<Calificacion> calificacionOptional = Optional.of(calificacion);
-        when(repositoryMock.findById(id)).thenReturn(calificacionOptional);
-        
         CalificacionDTO expectedDTO = new CalificacionDTO();
+        
+        when(repositoryMock.findById(id)).thenReturn(Optional.of(calificacion));
         when(modelMapperMock.map(calificacion, CalificacionDTO.class)).thenReturn(expectedDTO);
         
         CalificacionDTO resultDTO = service.get(id);
@@ -41,75 +54,76 @@ public class CalificacionServiceTest {
         assertEquals(expectedDTO, resultDTO);
     }
 
-    @Test(expected = NoSuchElementException.class)
+    @Test
     public void testGetCalificacionNotFound() {
         Long id = 2L;
         when(repositoryMock.findById(id)).thenReturn(Optional.empty());
         
-        service.get(id);
+        Exception exception = assertThrows(NoSuchElementException.class, () -> service.get(id));
+        
+        String expectedMessage = "No se encontró una Calificación con el ID: " + id;
+        String actualMessage = exception.getMessage();
+        
+        assertTrue(actualMessage.contains(expectedMessage));
     }
+
     @Test
     public void testSaveCalificacion() {
-    CalificacionDTO calificacionDTO = new CalificacionDTO();
-    // Configura los atributos necesarios de calificacionDTO
-    calificacionDTO.setId(null); // Simula un nuevo registro
-
-    Calificacion calificacion = new Calificacion();
-    // Configura los atributos necesarios de Calificacion, si es necesario
-    calificacion.setId(1L); // Simula el ID asignado por la base de datos
-
-    when(modelMapperMock.map(calificacionDTO, Calificacion.class)).thenReturn(calificacion);
-    when(repositoryMock.save(any(Calificacion.class))).thenReturn(calificacion);
-    when(modelMapperMock.map(calificacion, CalificacionDTO.class)).thenReturn(calificacionDTO);
-
-    CalificacionDTO resultDTO = service.save(calificacionDTO);
-
-    assertNotNull(resultDTO);
-    assertNotNull(resultDTO.getId());
-    assertEquals(Long.valueOf(1L), resultDTO.getId());
+        CalificacionDTO calificacionDTO = new CalificacionDTO();
+        Calificacion calificacion = new Calificacion();
+        calificacion.setId(1L);
+        
+        when(modelMapperMock.map(calificacionDTO, Calificacion.class)).thenReturn(calificacion);
+        when(repositoryMock.save(any(Calificacion.class))).thenReturn(calificacion);
+        
+        CalificacionDTO resultDTO = service.save(calificacionDTO);
+        
+        assertNotNull(resultDTO);
+        assertEquals(Long.valueOf(1L), resultDTO.getId());
     }
+
     @Test
     public void testUpdateCalificacionExists() {
         Long id = 1L;
         CalificacionDTO calificacionDTOToUpdate = new CalificacionDTO();
         calificacionDTOToUpdate.setId(id);
-
         Calificacion existingCalificacion = new Calificacion();
         existingCalificacion.setId(id);
-
+        
         when(repositoryMock.findById(id)).thenReturn(Optional.of(existingCalificacion));
         when(modelMapperMock.map(calificacionDTOToUpdate, Calificacion.class)).thenReturn(existingCalificacion);
         when(repositoryMock.save(existingCalificacion)).thenReturn(existingCalificacion);
         when(modelMapperMock.map(existingCalificacion, CalificacionDTO.class)).thenReturn(calificacionDTOToUpdate);
-
+        
         CalificacionDTO updatedDTO = service.update(calificacionDTOToUpdate);
-
+        
         assertNotNull(updatedDTO);
         assertEquals(id, updatedDTO.getId());
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void testUpdateCalificacionNotFound() {
         Long id = 1L;
         CalificacionDTO calificacionDTOToUpdate = new CalificacionDTO();
         calificacionDTOToUpdate.setId(id);
-
+        
         when(repositoryMock.findById(id)).thenReturn(Optional.empty());
-
-        service.update(calificacionDTOToUpdate);
+        
+        Exception exception = assertThrows(RuntimeException.class, () -> service.update(calificacionDTOToUpdate));
+        
+        String expectedMessage = "Registro no encontrado";
+        String actualMessage = exception.getMessage();
+        
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     public void testDeleteCalificacion() {
         Long id = 1L;
-        
         doNothing().when(repositoryMock).deleteById(id);
         
         service.delete(id);
         
-        verify(repositoryMock, times(1)).deleteById(id);
+        verify(repositoryMock).deleteById(id);
     }
-
-
-
 }
