a510bf233ec05274975e8902ab3e494d5ec7b3f2
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2024-05-26 18:21:48 -0500

    add: response status

--- src/main/java/com/backend/backend_web/exception/RegistroNoEncontradoException.java
+++ src/main/java/com/backend/backend_web/exception/RegistroNoEncontradoException.java
@@ -3,8 +3,8 @@ package com.backend.backend_web.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class RegistroNoEncontradoException extends Exception {
-    // private static final long serialVersionUID = 1L;
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RegistroNoEncontradoException extends RuntimeException {
     public RegistroNoEncontradoException(String message) {
         super(message);
     }
